ec8b255b0f7606ee48269f7893e6f0376f16cd70
Super Zombi <rost201541@gmail.com>
2022-08-03 21:42:37 +0300

    Update script.js

--- chrome/script.js
+++ chrome/script.js
@@ -5,23 +5,27 @@ chrome.storage.sync.get({ download: true,
 	if (results.hideVK){
 		hideVK()
 	}
-	if (results.download){
-		window.onload = function(){
-			let chrome_arr = {
-				downloadStr: chrome.i18n.getMessage("downloadStr"),
-				downloadLinkDesc: chrome.i18n.getMessage("downloadLinkDesc"),
-				subtitles: chrome.i18n.getMessage("subtitles"),
+	let player = document.getElementById('player')
+	if (player){
+		document.title = document.querySelector('.b-content__main .b-post__title').innerText;
 
-				args: {
-					subtitles: results.subtitles
+		if (results.download){
+			window.onload = function(){
+				let chrome_arr = {
+					downloadStr: chrome.i18n.getMessage("downloadStr"),
+					downloadLinkDesc: chrome.i18n.getMessage("downloadLinkDesc"),
+					subtitles: chrome.i18n.getMessage("subtitles"),
+
+					args: {
+						subtitles: results.subtitles
+					}
 				}
+				const script = document.createElement('script');
+				script.src = chrome.runtime.getURL('myscript.js');
+				script.dataset.args = JSON.stringify({chrome_arr});
+				document.documentElement.appendChild(script);
 			}
-
-			const script = document.createElement('script');
-			script.src = chrome.runtime.getURL('myscript.js');
-			script.dataset.args = JSON.stringify({chrome_arr});
-			document.documentElement.appendChild(script);
-		}
+		}	
 	}
 });
 
