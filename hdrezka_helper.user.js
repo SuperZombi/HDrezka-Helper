3080629514ed57ee092978dd57f58b074e3c3759
Super Zombi <rost201541@gmail.com>
2022-08-03 21:47:08 +0300

    Update hdrezka_helper.user.js

--- hdrezka_helper.user.js
+++ hdrezka_helper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         HDrezka Helper
-// @version      3.1.0
+// @version      3.1.1
 // @description  Adds a «Download» button below the video. Export favorites and more.
 // @author       Super Zombi
 // @include      *rezka.*
@@ -161,7 +161,7 @@ GM_registerMenuCommand(get_message('settings'), ()=>{
 							  box-shadow: 1px 1px 10px grey; color: white;">
 	  ${get_message("saveBut")}</button>
 	<p style="display: flex; align-items: center; justify-content: space-between; margin-top: 1em">
-	  <a style="display: flex; align-items: center;" href="https://github.com/SuperZombi/HDrezka-downloader" target="_blank">
+	  <a style="display: flex; align-items: center;" href="https://github.com/SuperZombi/HDrezka-Helper" target="_blank">
 		<svg style="background: white; border-radius: 50px; padding: 1px;" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
 		<span style="margin-left:5px; color: blue;">GitHub</span>
 	  </a>
@@ -212,21 +212,25 @@ function main(){
 		hideVK()
 	}
 	var player = document.getElementById('player')
-	if (player && db_get("download", true)){
-		downloader();
-		var temp_video_src = player.getElementsByTagName("video")[0].src
+	if (player){
+		document.title = document.querySelector('.b-content__main .b-post__title').innerText;
 
-		let observer = new MutationObserver(function(mutations) {
-			mutations.forEach(function(mutation) {
-				if (temp_video_src != document.getElementById('player').getElementsByTagName("video")[0].src) {
-					temp_video_src = document.getElementById('player').getElementsByTagName("video")[0].src
-					downloader();
-				}
+		if (db_get("download", true)){
+			downloader();
+			var temp_video_src = player.getElementsByTagName("video")[0].src
+
+			let observer = new MutationObserver(function(mutations) {
+				mutations.forEach(function(mutation) {
+					if (temp_video_src != document.getElementById('player').getElementsByTagName("video")[0].src) {
+						temp_video_src = document.getElementById('player').getElementsByTagName("video")[0].src
+						downloader();
+					}
+				});
 			});
-		});
-		observer.observe(document.querySelector("body"), {childList: true, subtree: true});
+			observer.observe(document.querySelector("body"), {childList: true, subtree: true});
+		}
 	}
-	if (window.location.pathname.includes("/favorites/") && db_get("exportFavorites", true)){
+	else if (window.location.pathname.includes("/favorites/") && db_get("exportFavorites", true)){
 		exportFavorites()
 	}
 }
