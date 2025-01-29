dc9e35d3dcda78890a2a97481183a2a74c9a21e6
Super Zombi <super.zombi.yt@gmail.com>
2025-01-29 20:01:53 +0200

    localize without innerHTML

--- src/menu/localize.js
+++ src/menu/localize.js
@@ -1,23 +1,10 @@
 var browser = chrome || browser;
-function localizeHtmlPage()
-{
-    //Localize by replacing __MSG_***__ meta tags
-    var objects = document.getElementsByTagName('html');
-    for (var j = 0; j < objects.length; j++)
-    {
-        var obj = objects[j];
-
-        var valStrH = obj.innerHTML.toString();
-        var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function(match, v1)
-        {
-            return v1 ? browser.i18n.getMessage(v1) : "";
-        });
-
-        if(valNewH != valStrH)
-        {
-            obj.innerHTML = valNewH;
-        }
-    }
+function localizeHtmlPage() {
+    document.querySelectorAll('[localize]').forEach(elem => {
+        elem.innerText = browser.i18n.getMessage(elem.getAttribute("localize"))
+    })
+    document.querySelectorAll('[localize-title]').forEach(elem => {
+        elem.title = browser.i18n.getMessage(elem.getAttribute("localize-title"))
+    })
 }
-
 localizeHtmlPage();
